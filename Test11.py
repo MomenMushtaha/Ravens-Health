f1106be5f2072bdc30a4f10fa2fb2cba2903f431
Gineydi <81820393+Gineydi@users.noreply.github.com>
2023-03-06 16:12:53 -0500

    Update Test11.py

--- Test11.py
+++ Test11.py
@@ -1,61 +1,88 @@
 import csv 
 import sqlite3 
 
-connection = sqlite3.connect('fitbitdatatry2.db')
+connection = sqlite3.connect('databa.db')
 cur = connection.cursor() 
-cur.execute('''CREATE TABLE Activities
-          (Date TEXT,
-          Calories_Burned TEXT, 
-          Steps TEXT,
-          Distance TEXT, 
-          Floors TEXT,
-          Minutes_Sedentary TEXT,
-          Minutes_Lightly_Active TEXT,  
-          Minutes_Fairly_Active TEXT, 
-          Minutes_Very_Active TEXT, 
-          Activity_Calories TEXT)
+cur.execute('''CREATE TABLE health
+          (Dates text,
+          BURN_CAL integer, 
+          STEPS integer, 
+          DISTANCE integer, 
+          FLOORS integer, 
+          SED_MINS integer,
+          L_AC_MINS integer, 
+          F_AC_MINS integer,
+          V_AC_MINS integer,
+          AC_CAL integer,  
+          ASLEEP_MINS integer, 
+          AWAKE_MINS integer, 
+          AWAKE_NUM integer, 
+          TIME_BED integer, 
+          REM_SLEEP integer, 
+          LIGHT_SLEEP integer, 
+          DEEP_SLEEP integer)
          ''')
-cur.execute ('''CREATE TABLE  Sleep
-          (Start_Time TEXT, 
-          End_Time TEXT, 
-          Minutes_Asleep TEXT, 
-          Minutes_Awake TEXT, 
-          Number_of_Awakenings TEXT, 
-          Time_in_Bed TEXT, 
-          Minutes_REM_Sleep TEXT,
-          Minutes_Light_Sleep TEXT, 
-          Minutes_Deep_Sleep TEXT)
-        ''')
+         
+a = 0; 
+s = 0; 
+d = 0; 
+data = 0; 
+
 fname= input('Enter the csv file name:')
 if len(fname) < 1: fname = 'Database.csv'
-with open(fname) as csv_file: 
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    for row in csv_reader: 
-        print (row)
-        insert_activities = '''INSERT INTO Activities (Date,
-        Calories_Burned,
-          Steps, 
-          Distance , 
-          Floors ,
-          Minutes_Sedentary ,
-          Minutes_Lightly_Active ,  
-          Minutes_Fairly_Active , 
-          Minutes_Very_Active , 
-          Activity_Calories ) VALUES (?, ?, ?,?, ?, ?, ?, ?, ?,?) '''
-        instert_sleep = '''INSERT INTO Sleep (Start_Time, 
-          End_Time, 
-          Minutes_Asleep, 
-          Minutes_Awake, 
-          Number_of_Awakenings, 
-          Time_in_Bed, 
-          Minutes_REM_Sleep,
-          Minutes_Light_Sleep, 
-          Minutes_Deep_Sleep) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?) '''
-        cur.executemany(insert_activities, csv_reader)
-        cur.executemany(instert_sleep, csv_reader)
-        select_act = '''SELECT * FROM Activities'''
-        select_sleep = '''SELECT * FROM Sleep'''
-        r = cur.execute(select_act).fetchall()
-        r1 = cur.execute(select_sleep).fetchall
+with open(fname, 'r') as csv_file: 
+  csv_reader = csv.reader(csv_file)
+  for i, row in enumerate(csv_reader): 
+        if 'Activities' in row: 
+          j = row.index('Activities')
+          a = i; 
+          # print (f'cell found at row {i}, column {j}')
+          #print (row)
+        if 'Sleep' in row: 
+          k = row.index('Sleep')  
+          s = i; 
+          # print (f'cell found at row {i}, column {k}')
+        if 'Daily Totals' in row: 
+          r = row.index('Daily Totals')
+          d = i; 
+          #print (f'cell found at row {i}, column {d}')
+          break 
+
+
+with open(fname, 'r') as csv_file: 
+  csv_reader = csv.reader(csv_file)
+  for j, row in enumerate(csv_reader): 
+      if j > (a+1) and j < (s-1): 
+        activitydata = (row[0], row[1], row[2], row[3], row[4],row[5], row[6], row[7], row[8], row[9] )
+        cur.execute('''INSERT INTO health(
+          Dates, 
+          BURN_CAL, 
+          STEPS, 
+          DISTANCE, 
+          FLOORS, 
+          SED_MINS, 
+          L_AC_MINS, 
+          F_AC_MINS,
+          V_AC_MINS, 
+          AC_CAL) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ? ) ''', activitydata); 
+
+with open(fname, 'r') as csv_file: 
+  csv_reader = csv.reader(csv_file)
+  for k, row in enumerate(csv_reader): 
+      if k > (s+1) and k <(d-2): 
+        # print (row[0])
+        sleepdata = (row[2], row[3], row[4], row[5], row[6], row[7], row[8])
+        cur.execute('''INSERT INTO health(
+          ASLEEP_MINS , 
+          AWAKE_MINS , 
+          AWAKE_NUM , 
+          TIME_BED , 
+          REM_SLEEP , 
+          LIGHT_SLEEP , 
+          DEEP_SLEEP) VALUES (?, ?, ?, ?, ?, ?, ?)''', sleepdata); 
+
+csv_file.close()
 connection.commit() 
 connection.close() 
+
+
