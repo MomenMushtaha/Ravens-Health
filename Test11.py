50455af78d5ad8ebcce23cd9217cab57f6036bdb
Gineydi <81820393+Gineydi@users.noreply.github.com>
2023-04-04 12:35:37 -0400

    Update Test11.py

--- Test11.py
+++ Test11.py
@@ -1,10 +1,41 @@
-import csv 
-import sqlite3 
+#Gineydi Orozco 101165733
+from cryptography.fernet import Fernet 
+import csv
+import sqlite3
 
-connection = sqlite3.connect('databa.db')
+#Creation of Key 
+key = Fernet.generate_key() 
+
+#opens the key
+with open('filekey.key', 'wb') as filekey: 
+    filekey.write(key)
+
+with open('filekey.key', 'rb') as filekey: 
+    key = filekey.read() 
+
+#Using the key 
+fernet = Fernet(key)
+
+#Enter the csv file name 
+fname= input('Enter the csv file name:')
+if len(fname) < 1: 
+    fname = 'Database.csv'
+
+with open(fname, 'r') as csv_file: 
+    csv_contents = csv_file.read()
+
+#Encrypt the file 
+encrypted = fernet.encrypt(csv_contents.encode())
+
+
+with open(f'{fname}.encrypted', 'wb') as encrypted_file: 
+    encrypted_file.write(encrypted)
+
+#Creating Database 
+connection = sqlite3.connect('database.db')
 cur = connection.cursor() 
-cur.execute('''CREATE TABLE health
-          (Dates text,
+cur.execute('''CREATE TABLE activity 
+          (Dates datetime,
           BURN_CAL integer, 
           STEPS integer, 
           DISTANCE integer, 
@@ -13,48 +44,58 @@ cur.execute('''CREATE TABLE health
           L_AC_MINS integer, 
           F_AC_MINS integer,
           V_AC_MINS integer,
-          AC_CAL integer,  
+          AC_CAL integer)
+         ''')
+
+cur.execute('''CREATE TABLE sleep
+          (Dates datetime, 
           ASLEEP_MINS integer, 
           AWAKE_MINS integer, 
           AWAKE_NUM integer, 
           TIME_BED integer, 
           REM_SLEEP integer, 
           LIGHT_SLEEP integer, 
-          DEEP_SLEEP integer)
-         ''')
-         
-a = 0; 
-s = 0; 
-d = 0; 
-data = 0; 
+          DEEP_SLEEP integer) 
+        ''')
 
-fname= input('Enter the csv file name:')
-if len(fname) < 1: fname = 'Database.csv'
-with open(fname, 'r') as csv_file: 
+#Variables for row indexes 
+a = 0
+s = 0 
+d = 0 
+data = 0 
+
+#Decrypt the file, should be in the actual database program 
+fernet = Fernet(key)
+
+#Open Encrypted file
+with open(f'{fname}.encrypted', 'rb') as enc_file: 
+    encrypted = enc_file.read()
+
+decrypted = fernet.decrypt(encrypted)
+
+with open(f'{fname}.decrypted', 'wb') as dec_file: 
+    dec_file.write(decrypted)
+
+with open(f'{fname}.decrypted', 'r') as csv_file: 
   csv_reader = csv.reader(csv_file)
   for i, row in enumerate(csv_reader): 
         if 'Activities' in row: 
           j = row.index('Activities')
           a = i; 
-          # print (f'cell found at row {i}, column {j}')
-          #print (row)
         if 'Sleep' in row: 
           k = row.index('Sleep')  
           s = i; 
-          # print (f'cell found at row {i}, column {k}')
         if 'Daily Totals' in row: 
           r = row.index('Daily Totals')
           d = i; 
-          #print (f'cell found at row {i}, column {d}')
           break 
-
-
-with open(fname, 'r') as csv_file: 
+  
+with open(f'{fname}.decrypted', 'r') as csv_file: 
   csv_reader = csv.reader(csv_file)
   for j, row in enumerate(csv_reader): 
       if j > (a+1) and j < (s-1): 
         activitydata = (row[0], row[1], row[2], row[3], row[4],row[5], row[6], row[7], row[8], row[9] )
-        cur.execute('''INSERT INTO health(
+        cur.execute('''INSERT INTO activity(
           Dates, 
           BURN_CAL, 
           STEPS, 
@@ -66,20 +107,33 @@ with open(fname, 'r') as csv_file:
           V_AC_MINS, 
           AC_CAL) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ? ) ''', activitydata); 
 
-with open(fname, 'r') as csv_file: 
+with open(f'{fname}.decrypted', 'r') as csv_file: 
   csv_reader = csv.reader(csv_file)
   for k, row in enumerate(csv_reader): 
       if k > (s+1) and k <(d-2): 
-        # print (row[0])
-        sleepdata = (row[2], row[3], row[4], row[5], row[6], row[7], row[8])
-        cur.execute('''INSERT INTO health(
+
+        sleepdata = (row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8])
+        cur.execute('''INSERT INTO sleep(
+          Dates, 
           ASLEEP_MINS , 
           AWAKE_MINS , 
           AWAKE_NUM , 
           TIME_BED , 
           REM_SLEEP , 
           LIGHT_SLEEP , 
-          DEEP_SLEEP) VALUES (?, ?, ?, ?, ?, ?, ?)''', sleepdata); 
+          DEEP_SLEEP) VALUES (?, ?, ?, ?, ?, ?, ?, ?)''', sleepdata); 
+        #Joining Both Tables    
+        cur.execute('''
+          SELECT activity.*, sleep.ASLEEP_MINS, sleep.AWAKE_MINS, sleep.AWAKE_NUM, sleep.TIME_BED, sleep.REM_SLEEP, sleep.LIGHT_SLEEP, sleep.DEEP_SLEEP
+          FROM activity
+          JOIN sleep ON substr(sleep.Dates, 1, 10) = activity.Dates
+          ''')
+
+#Encryption 
+encrypted = fernet.encrypt(csv_contents.encode())
+
+with open(f'{fname}.encrypted', 'wb') as encrypted_file: 
+    encrypted_file.write(encrypted)
 
 csv_file.close()
 connection.commit() 
